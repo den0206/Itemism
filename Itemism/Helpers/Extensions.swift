f4173c3d53501133634cabf0af93bcb14d907004
den0206 <mac.pop@mineo.jp>
2020-07-11 17:56:16 +0900

    custom extension

--- Itemism/Helpers/Extensions.swift
+++ Itemism/Helpers/Extensions.swift
@@ -13,27 +13,6 @@ public struct AnchoredConstraints {
     public var top, leading, bottom, trailing, width, height: NSLayoutConstraint?
 }
 
-class GradiatinButton : UIButton {
-    override func draw(_ rect: CGRect) {
-        super.draw(rect)
-
-        let gradientLayer = CAGradientLayer()
-        let leftColor = #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1)
-        let rightColor = #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1)
-
-
-        gradientLayer.colors = [leftColor.cgColor, rightColor.cgColor]
-        gradientLayer.startPoint = CGPoint(x: 0, y: 0.5)
-        gradientLayer.endPoint = CGPoint(x: 1, y: 0.5)
-
-        self.layer.insertSublayer(gradientLayer, at: 0)
-
-        layer.cornerRadius = rect.height /  2
-        clipsToBounds = true
-        gradientLayer.frame = rect
-    }
-}
-
 func isValidEmail(_ string: String) -> Bool {
        let emailRegEx = "[A-Z0-9a-z._+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}"
        let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
--- Itemism/Helpers/GradientButton.swift
+++ Itemism/Helpers/GradientButton.swift
@@ -20,6 +20,8 @@ class GradientButton : UIButton {
         super.init(frame: .zero)
 
     }
+
+
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
